7c3f74fd6f932c938d12a4c0237ebb7f6d9f2c39
Mike <mylee13845@gmail.com>
2024-06-27 02:32:14 -0400

    database sql script added

---
+++ database/script/tables.sql
@@ -0,0 +1,15 @@
+-- create database SimplyQDB;
+ use SimplyQDB;
+
+show tables;
+-- drop table user;
+-- CREATE TABLE `user` (
+--   `uid` INT AUTO_INCREMENT PRIMARY KEY,
+--   `email` VARCHAR(255) NOT NULL,
+--   `name` VARCHAR(255),
+--   `password` VARCHAR(255),
+--   `inQueue` BOOLEAN
+-- );
+
+Select *
+From user
